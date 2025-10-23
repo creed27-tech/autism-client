2b1732a11f2a0d2bf6c91d3bd3bc61838a2f0a5d
creed27-tech <trestontyler27@gmail.com>
2025-10-22 22:56:47 -0400

    Remove and update all 1.21.4-incompatible legacy API/code
    Refactor Renderer2D to use a single Mesh for rendering and update methods for quad and triangle rendering.

--- src/main/java/meteordevelopment/meteorclient/renderer/Renderer2D.java
+++ src/main/java/meteordevelopment/meteorclient/renderer/Renderer2D.java
@@ -5,26 +5,25 @@
 
 package meteordevelopment.meteorclient.renderer;
 
-import com.mojang.blaze3d.textures.GpuTextureView;
 import meteordevelopment.meteorclient.gui.renderer.packer.TextureRegion;
 import meteordevelopment.meteorclient.utils.PreInit;
 import meteordevelopment.meteorclient.utils.render.color.Color;
 import net.minecraft.client.MinecraftClient;
 
+import static meteordevelopment.meteorclient.MeteorClient.mc;
+
 public class Renderer2D {
     public static Renderer2D COLOR;
     public static Renderer2D TEXTURE;
 
+    private final Mesh mesh;
     private final boolean textured;
 
-    public final MeshBuilder triangles;
-    public final MeshBuilder lines;
+    private boolean building;
 
     public Renderer2D(boolean textured) {
+        this.mesh = new ShaderMesh(Shaders.POS_TEX_COLOR, DrawMode.Triangles, Mesh.Attrib.Vec2, Mesh.Attrib.Vec2, Mesh.Attrib.Color);
         this.textured = textured;
-
-        triangles = new MeshBuilder(textured ? MeteorRenderPipelines.UI_TEXTURED : MeteorRenderPipelines.UI_COLORED);
-        lines = new MeshBuilder(MeteorRenderPipelines.UI_COLORED_LINES);
     }
 
     @PreInit
@@ -33,156 +32,136 @@ public class Renderer2D {
         TEXTURE = new Renderer2D(true);
     }
 
-    public void setAlpha(double alpha) {
-        triangles.alpha = alpha;
-    }
-
     public void begin() {
-        triangles.begin();
-        lines.begin();
+        if (building) throw new IllegalStateException("Renderer2D.begin() called twice");
+
+        building = true;
+        mesh.begin();
     }
 
     public void end() {
-        triangles.end();
-        lines.end();
+        if (!building) throw new IllegalStateException("Renderer2D.end() called without calling Renderer2D.begin()");
+
+        mesh.end();
+        building = false;
     }
 
-    public void render() {
-        render(null, null);
+    public void render(TextureRegion texture) {
+        if (!textured) throw new IllegalStateException("Rendering with a texture while using non textured renderer");
+
+        Texture.MISSING.bindTexture();
+        if (texture != null) texture.bind();
+
+        mesh.render();
     }
 
-    public void render(GpuTextureView textureView) {
-        if (!textured)
-            throw new IllegalStateException("Tried to render with a texture with a non-textured Renderer2D");
-
-        render("u_Texture", textureView);
-    }
-
-    public void render(String samplerName, GpuTextureView samplerView) {
-        if (lines.isBuilding()) lines.end();
-        if (triangles.isBuilding()) triangles.end();
-
-        MeshRenderer.begin()
-            .attachments(MinecraftClient.getInstance().getFramebuffer())
-            .pipeline(MeteorRenderPipelines.UI_COLORED_LINES)
-            .mesh(lines)
-            .end();
-
-        MeshRenderer.begin()
-            .attachments(MinecraftClient.getInstance().getFramebuffer())
-            .pipeline(textured ? MeteorRenderPipelines.UI_TEXTURED : MeteorRenderPipelines.UI_COLORED)
-            .mesh(triangles)
-            .sampler(samplerName, samplerView)
-            .end();
-    }
-
-    // Tris
-    public void triangle(double x1, double y1, double x2, double y2, double x3, double y3, Color color) {
-        triangles.ensureTriCapacity();
-
-        triangles.triangle(
-            triangles.vec2(x1, y1).color(color).next(),
-            triangles.vec2(x2, y2).color(color).next(),
-            triangles.vec2(x3, y3).color(color).next()
+    public void quad(double x, double y, double width, double height, Color cTopLeft, Color cTopRight, Color cBottomRight, Color cBottomLeft, TextureRegion texture) {
+        quad(
+                x, y,
+                x + width, y,
+                x + width, y + height,
+                x, y + height,
+                cTopLeft, cTopRight, cBottomRight, cBottomLeft,
+                texture
         );
     }
 
-    // Lines
+    public void quad(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, Color cTopLeft, Color cTopRight, Color cBottomRight, Color cBottomLeft, TextureRegion texture) {
+        double minU = textured ? texture.u1 : 0;
+        double minV = textured ? texture.v1 : 0;
+        double maxU = textured ? texture.u2 : 0;
+        double maxV = textured ? texture.v2 : 0;
 
-    public void line(double x1, double y1, double x2, double y2, Color color) {
-        lines.ensureLineCapacity();
-
-        lines.line(
-            lines.vec2(x1, y1).color(color).next(),
-            lines.vec2(x2, y2).color(color).next()
+        mesh.quad(
+            mesh.vec2(x1, y1).vec2(minU, minV).color(cTopLeft).next(),
+            mesh.vec2(x2, y2).vec2(maxU, minV).color(cTopRight).next(),
+            mesh.vec2(x3, y3).vec2(maxU, maxV).color(cBottomRight).next(),
+            mesh.vec2(x4, y4).vec2(minU, maxV).color(cBottomLeft).next()
         );
     }
 
-    public void boxLines(double x, double y, double width, double height, Color color) {
-        lines.ensureCapacity(4, 8);
-
-        int i1 = lines.vec2(x, y).color(color).next();
-        int i2 = lines.vec2(x, y + height).color(color).next();
-        int i3 = lines.vec2(x + width, y + height).color(color).next();
-        int i4 = lines.vec2(x + width, y).color(color).next();
-
-        lines.line(i1, i2);
-        lines.line(i2, i3);
-        lines.line(i3, i4);
-        lines.line(i4, i1);
-    }
-
-    // Quads
-
-    public void quad(double x, double y, double width, double height, Color cTopLeft, Color cTopRight, Color cBottomRight, Color cBottomLeft) {
-        triangles.ensureQuadCapacity();
-
-        triangles.quad(
-            triangles.vec2(x, y).color(cTopLeft).next(),
-            triangles.vec2(x, y + height).color(cBottomLeft).next(),
-            triangles.vec2(x + width, y + height).color(cBottomRight).next(),
-            triangles.vec2(x + width, y).color(cTopRight).next()
-        );
+    public void texture(double x, double y, double width, double height, TextureRegion texture) {
+        quad(x, y, width, height, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, texture);
     }
 
     public void quad(double x, double y, double width, double height, Color color) {
-        quad(x, y, width, height, color, color, color, color);
+        quad(x, y, width, height, color, color, color, color, null);
     }
 
-    // Textured quads
+    public void quad(double x, double y, double width, double height, Color cTopLeft, Color cTopRight, Color cBottomRight, Color cBottomLeft) {
+        quad(x, y, width, height, cTopLeft, cTopRight, cBottomRight, cBottomLeft, null);
+    }
 
-    public void texQuad(double x, double y, double width, double height, Color color) {
-        triangles.ensureQuadCapacity();
+    public void quad(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, Color color) {
+        quad(x1, y1, x2, y2, x3, y3, x4, y4, color, color, color, color, null);
+    }
 
-        triangles.quad(
-            triangles.vec2(x, y).vec2(0, 0).color(color).next(),
-            triangles.vec2(x, y + height).vec2(0, 1).color(color).next(),
-            triangles.vec2(x + width, y + height).vec2(1, 1).color(color).next(),
-            triangles.vec2(x + width, y).vec2(1, 0).color(color).next()
+    public void tri(double x1, double y1, double x2, double y2, double x3, double y3, Color color) {
+        tri(x1, y1, x2, y2, x3, y3, color, color, color, null);
+    }
+
+    public void tri(double x1, double y1, double x2, double y2, double x3, double y3, Color c1, Color c2, Color c3, TextureRegion texture) {
+        double minU = textured ? texture.u1 : 0;
+        double minV = textured ? texture.v1 : 0;
+        double maxU = textured ? texture.u2 : 0;
+        double maxV = textured ? texture.v2 : 0;
+
+        mesh.tri(
+            mesh.vec2(x1, y1).vec2(minU, minV).color(c1).next(),
+            mesh.vec2(x2, y2).vec2(maxU, minV).color(c2).next(),
+            mesh.vec2(x3, y3).vec2(maxU, maxV).color(c3).next()
         );
     }
 
-    public void texQuad(double x, double y, double width, double height, TextureRegion texture, Color color) {
-        triangles.ensureQuadCapacity();
+    public void circleOutline(double x, double y, double r, int segments, double width, Color color) {
+        double circumference = 2 * Math.PI * r;
+        double step = circumference / segments;
 
-        triangles.quad(
-            triangles.vec2(x, y).vec2(texture.x1, texture.y1).color(color).next(),
-            triangles.vec2(x, y + height).vec2(texture.x1, texture.y2).color(color).next(),
-            triangles.vec2(x + width, y + height).vec2(texture.x2, texture.y2).color(color).next(),
-            triangles.vec2(x + width, y).vec2(texture.x2, texture.y1).color(color).next()
-        );
+        double angle = 0;
+        double lastY = y + r;
+        double lastX = x;
+
+        for (int i = 0; i < segments; i++) {
+            angle += step / r;
+
+            double sin = Math.sin(angle);
+            double cos = Math.cos(angle);
+
+            double newX = x + sin * r;
+            double newY = y + cos * r;
+
+            lineWidth(lastX, lastY, newX, newY, width, color);
+
+            lastX = newX;
+            lastY = newY;
+        }
     }
 
-    public void texQuad(double x, double y, double width, double height, double rotation, double texX1, double texY1, double texX2, double texY2, Color color) {
-        triangles.ensureQuadCapacity();
-
-        double rad = Math.toRadians(rotation);
-        double cos = Math.cos(rad);
-        double sin = Math.sin(rad);
-
-        double oX = x + width / 2;
-        double oY = y + height / 2;
-
-        double _x1 = ((x - oX) * cos) - ((y - oY) * sin) + oX;
-        double _y1 = ((y - oY) * cos) + ((x - oX) * sin) + oY;
-        int i1 = triangles.vec2(_x1, _y1).vec2(texX1, texY1).color(color).next();
-
-        double _x2 = ((x - oX) * cos) - ((y + height - oY) * sin) + oX;
-        double _y2 = ((y + height - oY) * cos) + ((x - oX) * sin) + oY;
-        int i2 = triangles.vec2(_x2, _y2).vec2(texX1, texY2).color(color).next();
-
-        double _x3 = ((x + width - oX) * cos) - ((y + height - oY) * sin) + oX;
-        double _y3 = ((y + height - oY) * cos) + ((x + width - oX) * sin) + oY;
-        int i3 = triangles.vec2(_x3, _y3).vec2(texX2, texY2).color(color).next();
-
-        double _x4 = ((x + width - oX) * cos) - ((y - oY) * sin) + oX;
-        double _y4 = ((y - oY) * cos) + ((x + width - oX) * sin) + oY;
-        int i4 = triangles.vec2(_x4, _y4).vec2(texX2, texY1).color(color).next();
-
-        triangles.quad(i1, i2, i3, i4);
+    public void circle(double x, double y, double r, Color color) {
+        circle(x, y, r, 50, color);
     }
 
-    public void texQuad(double x, double y, double width, double height, double rotation, TextureRegion region, Color color) {
-        texQuad(x, y, width, height, rotation, region.x1, region.y1, region.x2, region.y2, color);
+    public void circle(double x, double y, double r, int segments, Color color) {
+        mesh.vec2(x, y).vec2(0, 0).color(color).next();
+
+        double step = 2 * Math.PI / segments;
+
+        for (int i = 0; i <= segments; i++) {
+            double angle = i * step;
+            double sin = Math.sin(angle);
+            double cos = Math.cos(angle);
+
+            mesh.vec2(x + sin * r, y + cos * r).vec2(0, 0).color(color).next();
+        }
+
+        mesh.triangleFan(0, segments + 1);
+    }
+
+    public void lineWidth(double x1, double y1, double x2, double y2, double width, Color color) {
+        double angle = Math.atan2(y2 - y1, x2 - x1) + Math.PI / 2;
+        double sin = Math.sin(angle) * width / 2;
+        double cos = Math.cos(angle) * width / 2;
+
+        quad(x1 + cos, y1 + sin, x2 + cos, y2 + sin, x2 - cos, y2 - sin, x1 - cos, y1 - sin, color);
     }
 }
