ac4e058229f2710f7bc4fd31b81ba54c12aae074
creed27-tech <trestontyler27@gmail.com>
2025-10-22 21:58:44 -0400

    Remove fabric-resource-loader-v1 (not present in Fabric API 1.21.4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,16 @@ plugins {
     id("fabric-loom") version "1.11-SNAPSHOT"
     id("maven-publish")
 }
-
 base {
     archivesName = properties["archives_base_name"] as String
     group = properties["maven_group"] as String
-
     val suffix = if (project.hasProperty("build_number")) {
         project.findProperty("build_number")
     } else {
         "local"
     }
-
     version = properties["minecraft_version"] as String + "-" + suffix
 }
-
 repositories {
     maven {
         name = "meteor-maven"
@@ -34,7 +30,6 @@ repositories {
         url = uri("https://repo.viaversion.com")
     }
     mavenCentral()
-
     exclusiveContent {
         forRepository {
             maven {
@@ -47,10 +42,8 @@ repositories {
         }
     }
 }
-
 val modInclude: Configuration by configurations.creating
 val jij: Configuration by configurations.creating
-
 configurations {
     // include mods
     modImplementation.configure {
@@ -59,7 +52,6 @@ configurations {
     include.configure {
         extendsFrom(modInclude)
     }
-
     // include libraries (jar-in-jar)
     implementation.configure {
         extendsFrom(jij)
@@ -68,17 +60,13 @@ configurations {
         extendsFrom(jij)
     }
 }
-
 dependencies {
     // Fabric
     minecraft("com.mojang:minecraft:${properties["minecraft_version"] as String}")
     mappings("net.fabricmc:yarn:${properties["yarn_mappings"] as String}:v2")
     modImplementation("net.fabricmc:fabric-loader:${properties["loader_version"] as String}")
-
     modInclude(fabricApi.module("fabric-api-base", properties["fapi_version"] as String))
     modInclude(fabricApi.module("fabric-resource-loader-v0", properties["fapi_version"] as String))
-    modInclude(fabricApi.module("fabric-resource-loader-v1", properties["fapi_version"] as String))
-
     // Compat fixes
     modCompileOnly(fabricApi.module("fabric-renderer-indigo", properties["fapi_version"] as String))
     modCompileOnly("maven.modrinth:sodium:${properties["sodium_version"] as String}") { isTransitive = false }
@@ -86,12 +74,10 @@ dependencies {
     modCompileOnly("maven.modrinth:iris:${properties["iris_version"] as String}") { isTransitive = false }
     modCompileOnly("com.viaversion:viafabricplus:${properties["viafabricplus_version"] as String}") { isTransitive = false }
     modCompileOnly("com.viaversion:viafabricplus-api:${properties["viafabricplus_version"] as String}") { isTransitive = false }
-
     // Baritone (https://github.com/MeteorDevelopment/baritone)
     modCompileOnly("meteordevelopment:baritone:${properties["baritone_version"] as String}-SNAPSHOT")
     // ModMenu (https://github.com/TerraformersMC/ModMenu)
     modCompileOnly("com.terraformersmc:modmenu:${properties["modmenu_version"] as String}")
-
     // Libraries
     jij("meteordevelopment:orbit:${properties["orbit_version"] as String}")
     jij("org.meteordev:starscript:${properties["starscript_version"] as String}")
@@ -101,53 +87,41 @@ dependencies {
     jij("io.netty:netty-codec-socks:${properties["netty_version"] as String}") { isTransitive = false }
     jij("de.florianmichael:WaybackAuthLib:${properties["waybackauthlib_version"] as String}")
 }
-
 // Handle transitive dependencies for jar-in-jar
 // Based on implementation from BaseProject by FlorianMichael/EnZaXD
 // Source: https://github.com/FlorianMichael/BaseProject/blob/main/src/main/kotlin/de/florianmichael/baseproject/Fabric.kt
 // Licensed under Apache License 2.0
 afterEvaluate {
     val jijConfig = configurations.findByName("jij") ?: return@afterEvaluate
-
     // Dependencies to exclude from jar-in-jar
     val excluded = setOf(
-        "org.slf4j",    // Logging provided by Minecraft
-        "jsr305"        // Compile time annotations only
+        "org.slf4j", // Logging provided by Minecraft
+        "jsr305" // Compile time annotations only
     )
-
-
     jijConfig.incoming.resolutionResult.allDependencies.forEach { dep ->
         val requested = dep.requested.displayName
-
         if (excluded.any { requested.contains(it) }) return@forEach
-
         val compileOnlyDep = dependencies.create(requested) {
             isTransitive = false
         }
-
         val implDep = dependencies.create(compileOnlyDep)
-
         dependencies.add("compileOnlyApi", compileOnlyDep)
         dependencies.add("implementation", implDep)
         dependencies.add("include", compileOnlyDep)
     }
 }
-
 loom {
     accessWidenerPath = file("src/main/resources/meteor-client.accesswidener")
 }
-
 afterEvaluate {
     tasks.migrateMappings.configure {
         outputDir.set(project.file("src/main/java"))
     }
 }
-
 tasks {
     processResources {
         val buildNumber = project.findProperty("build_number")?.toString() ?: ""
         val commit = project.findProperty("commit")?.toString() ?: ""
-
         val propertyMap = mapOf(
             "version" to project.version,
             "build_number" to buildNumber,
@@ -155,45 +129,36 @@ tasks {
             "minecraft_version" to project.property("minecraft_version"),
             "loader_version" to project.property("loader_version")
         )
-
         inputs.properties(propertyMap)
         filesMatching("fabric.mod.json") {
             expand(propertyMap)
         }
     }
-
     jar {
         inputs.property("archivesName", project.base.archivesName.get())
-
         from("LICENSE") {
             rename { "${it}_${inputs.properties["archivesName"]}" }
         }
-
         // Launch sub project
         dependsOn(":launch:compileJava")
         from(project(":launch").layout.buildDirectory.dir("classes/java/main"))
-
         manifest {
             attributes["Main-Class"] = "meteordevelopment.meteorclient.Main"
         }
     }
-
     java {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
-
         if (System.getenv("CI")?.toBoolean() == true) {
             withSourcesJar()
             withJavadocJar()
         }
     }
-
     withType<JavaCompile> {
         options.release = 21
         options.compilerArgs.add("-Xlint:deprecation")
         options.compilerArgs.add("-Xlint:unchecked")
     }
-
     javadoc {
         with(options as StandardJavadocDocletOptions) {
             addStringOption("Xdoclint:none", "-quiet")
@@ -201,33 +166,27 @@ tasks {
             addStringOption("charSet", "UTF-8")
         }
     }
-
     build {
         if (System.getenv("CI")?.toBoolean() == true) {
             dependsOn("javadocJar")
         }
     }
 }
-
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
             artifactId = "meteor-client"
-
             version = properties["minecraft_version"] as String + "-SNAPSHOT"
         }
     }
-
     repositories {
         maven("https://maven.meteordev.org/snapshots") {
             name = "meteor-maven"
-
             credentials {
                 username = System.getenv("MAVEN_METEOR_ALIAS")
                 password = System.getenv("MAVEN_METEOR_TOKEN")
             }
-
             authentication {
                 create<BasicAuthentication>("basic")
             }
